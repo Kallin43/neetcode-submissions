f91cd1112dc854b2c6a78109fae8701142161002
Kallin43 <noel.kallingal@gmail.com>
2026-07-19 17:48:40 +0530

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int jump(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        int current = 0;
+        int farthest = 0;
+        for(int i = 0; i<n-1; i++){
+            farthest = Math.max(farthest, nums[i]+i);
+            if(current==i){
+                count++;
+                current = farthest;
+            }
+        }
+        return count;
+    }
+}
